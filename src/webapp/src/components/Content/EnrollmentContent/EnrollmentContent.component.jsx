dc8c983900a490e22ed584da8ee22eec4d15405d
John <John.Corwin@aa.com>
2021-06-24 13:38:03 -0500

    Add Styling to Enrollment Content component

--- src/webapp/src/components/Content/EnrollmentContent/EnrollmentContent.component.jsx
+++ src/webapp/src/components/Content/EnrollmentContent/EnrollmentContent.component.jsx
@@ -1,6 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 const EnrollmentContent = () => {
+  const [availableSeats, setAvailableSeats] = useState(2500);
+  const [costOfAttendance, setCostOfAttendance] = useState(30000);
+  const [scholarshipRecommendation, setScholarshipRecommendation] = useState(3000);
+  const [chinaLer, setChinaLer] = useState(80);
+  const [satMin, setSatMin] = useState(600);
+  const [satMax, setSatMax] = useState(1200);
+  const [toeflMin, setToeflMin] = useState(40);
+  const [toeflMax, setToeflMax] = useState(80);
+  const [ieltsMin, setIeltsMin] = useState(3);
+  const [ieltsMax, setIeltsMax] = useState(6);
+  const [recommendationLetters, setRecommendationLetters] = useState(true);
+
+  const handleLeftValue = (leftValue, rightValue, maxValue, fun) => {
+    var intValue = parseInt(leftValue);
+    var newLeftValue = (leftValue >= rightValue) ? parseInt(rightValue) - 1 : intValue;
+    fun(newLeftValue);
+  }
+
+  const handleRightValue = (rightValue, leftValue, maxValue, fun) => {
+    var intValue = parseInt(rightValue);
+    var newRightValue = (rightValue <= leftValue) ? parseInt(leftValue) + 1 : intValue;
+    fun(newRightValue);
+  }
+
+  const getStyles = (value, maxValue) => {
+    var remainingPercent = (value / maxValue) * 100;
+    var usedPercent = 100 - remainingPercent;
+
+    return {
+      right: usedPercent+"%",
+      left: "0%"
+    }
+  }
+
+  const getMultiStyles = (leftValue, rightValue, maxValue) => {
+    var leftPercent = (leftValue / maxValue) * 100;
+    var rightPercent = 100 - ((rightValue / maxValue) * 100);
+
+    return {
+      right: rightPercent+"%",
+      left: leftPercent+"%",
+    }
+  }
+
   return (
     <div className="stuff">
       <h1 className="page-title">Enrollment Criteria</h1>
@@ -61,33 +105,33 @@ const EnrollmentContent = () => {
             <div className="slider">
               <span className="multirange one">
                 <input className="range" type="range" min="0" max="0" value="0"></input>
-                <input className="range" type="range" min="0" max="5000" value="2500" js-hook="seats"></input>
-                <span className="bar"></span>
+                <input className="range" type="range" min="0" max="5000" value={availableSeats} onChange={e => setAvailableSeats(e.target.value)}></input>
+                <span className="bar" style={getStyles(availableSeats, 5000)}></span>
               </span>
-              <input type="text" value="2500" js-hook="seats-value"></input>
+              <input type="text" value={availableSeats}></input>
             </div>
             <h5>Cost of Atendance</h5>
             <div className="slider">
               <span className="multirange one">
                 <input className="range" type="range" min="0" max="0" value="0"></input>
-                <input className="range" type="range" min="0" max="60000" value="30000" js-hook="cost"></input>
-                <span className="bar"></span>
+                <input className="range" type="range" min="0" max="60000" value={costOfAttendance} onChange={e => setCostOfAttendance(e.target.value)}></input>
+                <span className="bar" style={getStyles(costOfAttendance, 60000)}></span>
               </span>
               <div className="dollar">
                 <span className="sign">$</span>
-                <input type="text" value="30,000" js-hook="cost-value"></input>
+                <input type="text" value={costOfAttendance}></input>
               </div>
             </div>
             <h5>Scholarship Recommendation</h5>
             <div className="slider">
               <span className="multirange one">
                 <input className="range" type="range" min="0" max="0" value="0"></input>
-                <input className="range" type="range" min="0" max="10000" value="3000" js-hook="scholarship"></input>
-                <span className="bar"></span>
+                <input className="range" type="range" min="0" max="10000" value={scholarshipRecommendation} onChange={e => setScholarshipRecommendation(e.target.value)}></input>
+                <span className="bar" style={getStyles(scholarshipRecommendation, 10000)}></span>
               </span>
               <div className="dollar">
                 <span className="sign">$</span>
-                <input type="text" value="3,000" js-hook="scholarship-value"></input>
+                <input type="text" value={scholarshipRecommendation}></input>
               </div>
             </div>
             <h5>Payment Deadline</h5>
@@ -142,46 +186,46 @@ const EnrollmentContent = () => {
               <div className="slider">
                 <span className="multirange one">
                   <input className="range" type="range" min="0" max="0" value="0"></input>
-                  <input className="range" type="range" min="0" max="100" value="80" js-hook="ler"></input>
-                  <span className="bar"></span>
+                  <input className="range" type="range" min="0" max="100" value={chinaLer} onChange={e => setChinaLer(e.target.value)}></input>
+                  <span className="bar" style={getStyles(chinaLer, 100)}></span>
                 </span>
                 <div className="dollar">
-                  <input className="ler-value" type="text" value="80" js-hook="ler-value"></input>
+                  <input className="ler-value" type="text" value={chinaLer}></input>
                   <span className="percent">%</span>
                 </div>
               </div>
             </h5>
             <h5>SAT Score</h5>
             <span className="multirange multi">
-              <input className="range" type="range" min="0" max="1600" value="600" js-hook="satmin"></input>
-              <input className="range" type="range" min="0" max="1600" value="1200" js-hook="satmax"></input>
-              <span className="bar"></span>
+              <input className="range" type="range" min="0" max="1600" value={satMin} onChange={e => handleLeftValue(e.target.value, satMax, 1600, setSatMin)}></input>
+              <input className="range" type="range" min="0" max="1600" value={satMax} onChange={e => handleRightValue(e.target.value, satMin, 1600, setSatMax)}></input>
+              <span className="bar" style={getMultiStyles(satMin, satMax, 1600)}></span>
             </span>
             <div className="values">
-              <input type="text" value="600" js-hook="satmin-value"></input>
-              <input type="text" value="1200" js-hook="satmax-value"></input>
+              <input type="text" value={satMin}></input>
+              <input type="text" value={satMax}></input>
               <div className="max">1600</div>
             </div>
             <h5>Toefl Score</h5>
             <span className="multirange multi">
-              <input className="range" type="range" min="0" max="120" value="40" js-hook="toeflmin"></input>
-              <input className="range" type="range" min="0" max="120" value="80" js-hook="toeflmax"></input>
-              <span className="bar"></span>
+              <input className="range" type="range" min="0" max="120" value={toeflMin} onChange={e => handleLeftValue(e.target.value, toeflMax, 120, setToeflMin)}></input>
+              <input className="range" type="range" min="0" max="120" value={toeflMax} onChange={e => handleRightValue(e.target.value, toeflMin, 120, setToeflMax)}></input>
+              <span className="bar" style={getMultiStyles(toeflMin, toeflMax, 120)}></span>
             </span>
             <div className="values">
-              <input type="text" value="40" js-hook="toeflmin-value"></input>
-              <input type="text" value="80" js-hook="toeflmax-value"></input>
+              <input type="text" value={toeflMin}></input>
+              <input type="text" value={toeflMax}></input>
               <div className="max">120</div>
             </div>
             <h5>IELTS Score</h5>
             <span className="multirange multi">
-              <input className="range" type="range" min="0" max="9" value="3" js-hook="ieltsmin"></input>
-              <input className="range" type="range" min="0" max="9" value="6" js-hook="ieltsmax"></input>
-              <span className="bar"></span>
+              <input className="range" type="range" min="0" max="9" value={ieltsMin} onChange={e => handleLeftValue(e.target.value, ieltsMax, 9, setIeltsMin)}></input>
+              <input className="range" type="range" min="0" max="9" value={ieltsMax} onChange={e => handleRightValue(e.target.value, ieltsMin, 9, setIeltsMax)}></input>
+              <span className="bar" style={getMultiStyles(ieltsMin, ieltsMax, 9)}></span>
             </span>
             <div className="values">
-              <input type="text" value="3" js-hook="ieltsmin-value"></input>
-              <input type="text" value="9" js-hook="ieltsmax-value"></input>
+              <input type="text" value={ieltsMin}></input>
+              <input type="text" value={ieltsMax}></input>
               <div className="max">9</div>
             </div>
             <h5>ACADEMIC RECOMMENDATION LETTERS?
